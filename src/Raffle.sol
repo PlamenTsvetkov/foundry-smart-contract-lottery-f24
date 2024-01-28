cc181f8555e147834a1562b1326e5c604a379bd2
PlamenCvetkov <cvetkov.plamen.1991@gmail.com>
2024-01-28 05:48:34 +0200

    Created custom error

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -30,19 +30,22 @@ pragma solidity ^0.8.18;
  * @dev Implements Chainlink VRFv2
  */
 contract Riffle {
+    error Raffle__NotEnoughEthSent();
+    
     uint256 private immutable i_entranceFee;
 
-    constructor(uint256 entranceFee){
-        i_entranceFee=entranceFee;
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
     }
 
-    function enterRaffle()  public payable{
-        
+    function enterRaffle() external payable {
+        // require(msg.value >= i_entranceFee, "Not enough ETH sent");
+        if(msg.value >= i_entranceFee){
+            revert Raffle__NotEnoughEthSent();
+        }
     }
-    
-    function pickWinnet()  public {
-        
-    }
+
+    function pickWinnet() public {}
 
     /** Getter Function */
     function getEntranceFee() external view returns (uint256) {
