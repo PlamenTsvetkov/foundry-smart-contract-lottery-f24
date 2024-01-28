687c809e7221c9918d0367a8aa3018e81bd3d448
PlamenCvetkov <cvetkov.plamen.1991@gmail.com>
2024-01-28 05:36:29 +0200

    Added solidity style guide

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,3 +1,24 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.18;
@@ -24,8 +45,7 @@ contract Riffle {
     }
 
     /** Getter Function */
-
-    function getEntranceFee()  external view returns (uint256) {
+    function getEntranceFee() external view returns (uint256) {
         return i_entranceFee;
     }
 }
